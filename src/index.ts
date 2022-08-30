7fafa6eef7a51ea799892b6341494f5dfef48e66
JusticHentai <1360636180@qq.com>
2022-08-30 17:12:28 +0800

    feat: cb

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,15 @@ import parseScriptCommand from './utils/parseScriptCommand'
 export interface Options {
   key: string // 挂载到 process.env[key]
   configKey: string // 挂载到 vite config 里
+  cb: (options: { env: string; event: string; script: string }) => any // 返回给 vite 配置
 }
 
 export default function simpleEnv(options: Options): any {
-  const { key, configKey } = options
+  const { key, configKey, cb } = options
 
   const { env, event, script } = parseScriptCommand()
+
+  const cbRes = cb({ env, event, script })
 
   return {
     name: 'vite-plugin-simple-env',
@@ -25,6 +28,7 @@ export default function simpleEnv(options: Options): any {
           script,
         },
       },
+      ...cbRes,
     }),
   }
 }
