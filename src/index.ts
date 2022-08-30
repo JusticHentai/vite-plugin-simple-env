88c0a16c6637632034a512188e1908f7cb85fcaa
JusticHentai <1360636180@qq.com>
2022-08-30 18:26:52 +0800

    feat: ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import parseScriptCommand from './utils/parseScriptCommand'
 
 export interface Options {
-  key: string // 挂载到 process.env[key]
-  configKey: string // 挂载到 vite config 里
-  cb: (options: { env: string; event: string; script: string }) => any // 返回给 vite 配置
+  key?: string // 挂载到 process.env[key]
+  configKey?: string // 挂载到 vite config 里
+  cb?: (options: { env: string; event: string; script: string }) => any // 返回给 vite 配置
 }
 
 export default function simpleEnv(options: Options): any {
@@ -11,24 +11,35 @@ export default function simpleEnv(options: Options): any {
 
   const { env, event, script } = parseScriptCommand()
 
-  const cbRes = cb({ env, event, script })
+  const cbRes = cb && cb({ env, event, script })
 
-  return {
-    name: 'vite-plugin-simple-env',
-    enforce: 'pre',
-    config: () => ({
+  const resList = [
+    key && {
       define: {
         'process.env': JSON.stringify({
           [key]: env,
         }),
       },
+    },
+    configKey && {
       [configKey]: {
         [event]: {
           env,
           script,
         },
       },
-      ...cbRes,
+    },
+    cbRes,
+  ].filter(Boolean)
+
+  // @ts-ignore object
+  const res = Object.assign(...resList)
+
+  return {
+    name: 'vite-plugin-simple-env',
+    enforce: 'pre',
+    config: () => ({
+      ...res,
     }),
   }
 }
